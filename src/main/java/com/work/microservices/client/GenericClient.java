bc9e26e5eab6787dbbbf142cf037d6954b798429
nunofontes <nunof.h.fontes@gmail.com>
2020-01-02 20:26:11 +0000

    RestTemplate GET method

--- src/main/java/com/work/microservices/client/GenericClient.java
+++ src/main/java/com/work/microservices/client/GenericClient.java
@@ -1,20 +1,28 @@
 package com.work.microservices.client;
 
+import com.work.microservices.web.model.BeerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.UUID;
+
 @Component
 @ConfigurationProperties(value="com.work.microservices", ignoreInvalidFields = false)
 public class GenericClient {
 
-    public final String BEER_PATH = "/api/v1/beer";
+    public final String BEER_PATH_V1 = "/api/v1/beer";
     private String apihost;
+
     private final RestTemplate restTemplate;
 
     public GenericClient(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
     }
 
     public void setApihost(String apihost) {
