d8e6e8e58c68b178b60204dbc17ad54c3ab43d29
nunofontes <nunof.h.fontes@gmail.com>
2020-01-02 20:21:06 +0000

    RestTemplate Builder added

--- src/main/java/com/work/microservices/client/GenericClient.java
+++ src/main/java/com/work/microservices/client/GenericClient.java
@@ -1,13 +1,21 @@
 package com.work.microservices.client;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 @ConfigurationProperties(value="com.work.microservices", ignoreInvalidFields = false)
 public class GenericClient {
 
+    public final String BEER_PATH = "/api/v1/beer";
     private String apihost;
+    private final RestTemplate restTemplate;
+
+    public GenericClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
 
     public void setApihost(String apihost) {
         this.apihost = apihost;
